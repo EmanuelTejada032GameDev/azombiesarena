6e2b54b73b5e8fe3315709ae20c824b78e555853
EmanuelTejada032GameDev <84829473+EmanuelTejada032GameDev@users.noreply.github.com>
2026-08-20 22:32:31 -0400

    Wave spawner Basic zombie and pathfinding using nav mesh

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameObject _zombiePrefab;
+    [SerializeField] private Transform[] _spawnEntrances; 
+    [SerializeField] private Transform _playerTarget;     
+
+    [Header("Wave Configuration")]
+    [SerializeField] private int _baseZombieCount = 5;
+    [SerializeField] private int _zombiesPerWaveMultiplier = 3;
+    [SerializeField] private float _timeBetweenSpawns = 1.5f;
+    [SerializeField] private float _intermissionDuration = 5.0f;
+    [SerializeField] private int _maxActiveZombiesCap = 24;
+
+    private int _currentWave = 0;
+    private int _totalZombiesForCurrentWave;
+    private int _zombiesSpawnedSoFar;
+    private int _currentActiveZombiesCount;
+    private bool _isIntermission = false;
+
+    private void Start()
+    {
+        StartNextWave();
+    }
+
+    private void StartNextWave()
+    {
+        _currentWave++;
+        _zombiesSpawnedSoFar = 0;
+        _currentActiveZombiesCount = 0;
+
+        _totalZombiesForCurrentWave = _baseZombieCount + (_currentWave * _zombiesPerWaveMultiplier);
+
+        //Debug.Log($"Wave {_currentWave} Started! Total Zombies: {_totalZombiesForCurrentWave}");
+
+        StartCoroutine(SpawnWaveRoutine());
+    }
+
+    private IEnumerator SpawnWaveRoutine()
+    {
+        while (_zombiesSpawnedSoFar < _totalZombiesForCurrentWave)
+        {
+            if (_currentActiveZombiesCount < _maxActiveZombiesCap)
+            {
+                SpawnZombie();
+                _zombiesSpawnedSoFar++;
+                _currentActiveZombiesCount++;
+
+                yield return new WaitForSeconds(_timeBetweenSpawns);
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+
+        while (_currentActiveZombiesCount > 0)
+        {
+            yield return null;
+        }
+
+        StartCoroutine(IntermissionRoutine());
+    }
+
+    private void SpawnZombie()
+    {
+        int randomGateIndex = Random.Range(0, _spawnEntrances.Length);
+        Transform chosenGate = _spawnEntrances[randomGateIndex];
+
+        GameObject newZombie = Instantiate(_zombiePrefab, chosenGate.position, chosenGate.rotation);
+
+        Zombie zombieScript = newZombie.GetComponent<Zombie>();
+
+        if (zombieScript != null)
+        {
+            zombieScript.InitializeTarget(_playerTarget);
+        }
+
+        HealthSystem zombieHealth = newZombie.GetComponent<HealthSystem>();
+        if (zombieHealth != null)
+        {
+            zombieHealth.OnDied += HandleZombieDeath;
+        }
+    }
+
+    private void HandleZombieDeath(object sender, System.EventArgs e)
+    {
+        HealthSystem deadZombieHealth = sender as HealthSystem;
+        if (deadZombieHealth != null)
+        {
+            deadZombieHealth.OnDied -= HandleZombieDeath;
+        }
+        _currentActiveZombiesCount--;
+    }
+
+    private IEnumerator IntermissionRoutine()
+    {
+        _isIntermission = true;
+        //Debug.Log($"Wave Clear! Intermission started for {_intermissionDuration} seconds...");
+
+        yield return new WaitForSeconds(_intermissionDuration);
+
+        _isIntermission = false;
+        StartNextWave();
+    }
+}
--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -1,18 +1,76 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class Zombie : MonoBehaviour
 {
+    [Header("Navigation Settings")]
+    [SerializeField] private float _pathUpdateInterval = 0.2f;
+
+    private NavMeshAgent _agent;
+    private Transform _targetPlayer;
+    private Coroutine _trackingCoroutine;
+
     private HealthSystem _healthSystem;
 
-    private void Start()
+    private void Awake()
     {
+        _agent = GetComponent<NavMeshAgent>();
         _healthSystem = GetComponent<HealthSystem>();
-        _healthSystem.OnDied += HealthSystem_OnDied;
     }
 
-    private void HealthSystem_OnDied(object sender, EventArgs e)
+    private void OnEnable()
     {
-        Destroy(gameObject);
+        if (_healthSystem != null)
+        {
+            _healthSystem.OnDied += HandleDeath;
+        }
+    }
+
+    
+    public void InitializeTarget(Transform playerTransform)
+    {
+        _targetPlayer = playerTransform;
+
+        if (_targetPlayer != null)
+        {
+            _trackingCoroutine = StartCoroutine(TrackTargetRoutine());
+        }
+    }
+
+    private IEnumerator TrackTargetRoutine()
+    {
+        while (_targetPlayer != null)
+        {
+            _agent.SetDestination(_targetPlayer.position);
+            yield return new WaitForSeconds(_pathUpdateInterval);
+        }
+    }
+
+    private void HandleDeath(object sender, EventArgs e)
+    {
+        _healthSystem.OnDied -= HandleDeath;
+
+        if (_trackingCoroutine != null)
+        {
+            StopCoroutine(_trackingCoroutine);
+        }
+
+        if (_agent != null && _agent.isOnNavMesh)
+        {
+            _agent.isStopped = true;
+            _agent.enabled = false; 
+        }
+
+        // Trigger zombie death logic and FXs here
+        Destroy(gameObject, .4f);
+    }
+
+    private void OnDisable()
+    {
+        if (_healthSystem != null) _healthSystem.OnDied -= HandleDeath;
+        if (_trackingCoroutine != null) StopCoroutine(_trackingCoroutine);
     }
 }
